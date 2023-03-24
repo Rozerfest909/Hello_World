c58e9fa7166c5aa74719802259ae4a3fbf35add9
a.faron <rozerfest909@gmail.com>
2023-03-24 18:29:41 +0300

    5

---
+++ 5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "катя")
+{
+    Console.WriteLine("Ура! Это же Катя");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.Write(username);
+}
